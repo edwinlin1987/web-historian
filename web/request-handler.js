5ca31d54a7f04cb7de5a40996d4ba330eb3c50d5
edwinlin1987 <edwinlin1987@gmail.com>
2015-06-03 21:11:16 -0700

    typo

--- web/request-handler.js
+++ web/request-handler.js
@@ -22,7 +22,7 @@ exports.handleRequest = function (req, res) {
             res.end(data);
           });
         } else {
-          archive.isUrlInList(string);
+          archive.addUrlToList(string);
           fs.readFile(__dirname+'/public/loading.html', function (err, data) {
             res.writeHead(200, http.headers);
             res.end(data);
